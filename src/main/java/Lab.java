224c424662fa53589a94ca2ea52b98b06091ae02
Michael Smith <michaelsmith1994@yahoo.com>
2025-01-07 19:28:05 +0000

    Another lab completed

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,5 +1,7 @@
 
 import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 /**
  * Sets are unordered collections of elements. Sets do not allow duplicates.
@@ -19,7 +21,8 @@ public class Lab {
      * @return a new Set<Integer> object.
      */
     public Set<Integer> createSet(){
-        return null;
+        SortedSet<Integer> newSet = new TreeSet<>();
+        return newSet;
     }
 
     /**
@@ -28,7 +31,8 @@ public class Lab {
      * @return the size of set. (number of values)
      */
     public int getSize(Set<Integer> set){
-        return 0;
+        int setSize = set.size();
+        return setSize;
     }
 
     /**
@@ -38,7 +42,7 @@ public class Lab {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void addUniqueItem(Set<Integer> set, int item){
-
+        set.add(item);
     }
 
     /**
@@ -48,6 +52,6 @@ public class Lab {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void removeItem(Set<Integer> set, int item){
-
+        set.remove(item);
     }
 }
